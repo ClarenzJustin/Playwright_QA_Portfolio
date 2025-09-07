83a0cccab9b9c2b3b91fb440801940e292900dba
ClarenzJustin <clarenzjustinyumul.com>
2025-09-07 16:01:18 +0800

    Added login tests (valid and invalid login scenarios)

---
+++ tests/test_login.py
@@ -0,0 +1,18 @@
+import pytest
+from pages.login_page import LoginPage
+
+def test_valid_login(browser):
+    login_page = LoginPage(browser)
+    login_page.load()
+    login_page.login("standard_user", "secret_sauce")
+
+   
+    assert "inventory" in browser.url
+
+def test_invalid_login(browser):
+    login_page = LoginPage(browser)
+    login_page.load()
+    login_page.login("wrong_user", "wrong_pass")
+
+    
+    assert "Epic sadface" in login_page.get_error_message()
